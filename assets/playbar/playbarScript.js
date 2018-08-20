bf78d8332e92a35bdda7805f1a44f7d4e239d364
bjct <974362914@qq.com>
2018-08-20 11:46:17 +0800

    c

--- assets/playbar/playbarScript.js
+++ assets/playbar/playbarScript.js
@@ -15,10 +15,14 @@ cp.playbarAssetArr =
 	'Glow',
 	'GlowSmall',
 	'Height',
+	'InnerStroke',
+	'InnerStrokeSmall',
 	'Play',
 	'Pause',
 	'Progress',
 	'Rewind',
+	'Shade',
+	'ShadeSmall',
 	'Stroke',
 	'StrokeSmall',
 	'Thumb',
@@ -50,20 +54,6 @@ cp.playbarTooltips =
 	Thumb : 'Thumb',
 	ThumbBase : 'ThumbBase',
 	TOC : 'TOC'
-};
-cp.responsiveButtons = 
-{
-	//"ButtonName"	: 	[Primary,Tablet,Mobile],
-	"Rewind"		: 	[true,true,true,true,false],
-	"Backward"		: 	[true,true,true,true,true],
-	"Play"			: 	[true,true,true,true,true],
-	"Slider"		: 	[true,true,true,true,false],
-	"Forward"		: 	[true,true,true,true,true],
-	"CC"			: 	[true,true,true,true,true],
-	"AudioOn"		: 	[true,true,false,false,false],
-	"Exit"			: 	[true,true,true,true,true],
-	"FastForward"	: 	[true,true,true,true,false],
-	"TOC"			: 	[true,true,true,true,false]
 };
 cp.handleSpecialForPlaybar = function(playbarConstruct)
 {
